a6da74faa3c6be45f7a901de45e022a325499a3a
mucahit04 <mucahit04@gmail.com>
2020-05-23 17:54:58 +0200

    passed city obj instead of many props

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'> Weather</header>
-			{cityData.map((cityItem, index) => {
-				return <CityItem key={cityItem.id} cityName={cityItem.name} country={cityItem.sys.country} weatherMain={cityItem.weather[0].main} weatherDesc={cityItem.weather[0].description} latitude={cityItem.coord.lat} longitude={cityItem.coord.lon} temp_min={cityItem.main.temp_min} temp_max={cityItem.main.temp_max}
-        
-        />;
+			{cityData.map(cityItem => {
+				return <CityItem key={cityItem.id} city={cityItem} />;
 			})}
 		</div>
 	);
--- src/components/CityItem.js
+++ src/components/CityItem.js
@@ -1,17 +1,23 @@
 import React from "react";
 
-export default function CityItem(props) {
-	const { cityName, country,weatherMain, weatherDesc, longitude,latitude, temp_min,temp_max} = props;
-	return <div className="city-item">
-        <h2>{cityName}, {country}</h2>
-        <div className="weather-desc">
-        <p className="bold">{weatherMain}</p>
-        <p>{weatherDesc}</p>
-        </div>
-        <ul>
-        <li>min temp: {temp_min}</li>
-        <li>max temp: {temp_max}</li>
-        <li>location: {latitude}, {longitude}</li>
-        </ul>
-    </div>;
+export default function CityItem({ city }) {
+	const { name, sys, weather, main, coord } = city;
+	return (
+		<div className='city-item'>
+			<h2>
+				{name}, {sys.country}
+			</h2>
+			<div className='weather-desc'>
+				<p className='bold'>{weather[0].main}</p>
+				<p>{weather[0].description}</p>
+			</div>
+			<ul>
+				<li>min temp: {main.temp_min}</li>
+				<li>max temp: {main.temp_max}</li>
+				<li>
+					location: {coord.lat}, {coord.lon}
+				</li>
+			</ul>
+		</div>
+	);
 }
